e3254fcc9bb397866995010333ba42f7ba7cc751
Justin Hardage <justin.hardage@gmail.com>
2020-02-01 11:08:17 -0500

    Placeholder assets and organized folders

---
+++ Assets/Scripts/Topdownmovement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Topdownmovement : MonoBehaviour
+{
+    public Rigidbody2D rb2d;
+    public float speed = 5f;
+    
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 targetVel = (new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")));
+        rb2d.velocity = (targetVel * speed);
+    }
+}
