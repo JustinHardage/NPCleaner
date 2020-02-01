58404a2765d8671894874c5ab3ee35cc83e15ca6
Justin Hardage <justin.hardage@gmail.com>
2020-02-01 13:24:23 -0500

    Camera switch to new rooms

---
+++ Assets/Scripts/Room.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    public Cinemachine.CinemachineVirtualCamera vcam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // get player
+        // get cinemachine virtual camera
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay2D(Collider2D collider)
+    {
+        var player = collider.gameObject.GetComponent<Topdownmovement>();
+        Debug.Log($"player {player}");
+
+        if (player == null) { return; }
+
+        vcam.Follow = this.transform;
+    }
+
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        var player = collider.gameObject.GetComponent<Topdownmovement>();
+
+        if (player == null) { return; }
+
+        vcam.Follow = player.transform;
+    }
+}
