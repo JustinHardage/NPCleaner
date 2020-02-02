fc13fa8461c064123b33acb394d761a6e69894be
Justin Hardage <justin.hardage@gmail.com>
2020-02-02 14:10:43 -0500

    comitting before merge'

--- Assets/Scripts/BridgePhysics.cs
+++ Assets/Scripts/BridgePhysics.cs
@@ -7,11 +7,14 @@ public class BridgePhysics : MonoBehaviour {
 	//public static BridgePhysics bridgePhysics;
 	public static bool startTriggered = false;
 	public static bool endTriggered = false;
+    public static bool startTriggered2 = false;
+    public static bool endTriggered2 = false;
 	public GameObject baseWater;
-	public GameObject solvedWater;
-
+    public GameObject solvedWater;
     public Room parentRoom;
-
+    public Room parentRoom2;
+    public GameObject baseWater2;
+    public GameObject solvedWater2;
 
     // Use this for initialization
     void Start () {
@@ -26,7 +29,13 @@ public class BridgePhysics : MonoBehaviour {
 			solvedWater.SetActive (true);
             parentRoom.SetRoomState(Room.RoomState.SOLVED);
         }
-			
+        if (startTriggered2 && endTriggered2)
+        {
+            baseWater2.SetActive(false);
+            solvedWater2.SetActive(true);
+            parentRoom2.SetRoomState(Room.RoomState.SOLVED);
+        }
 
-	}
+
+    }
 }
